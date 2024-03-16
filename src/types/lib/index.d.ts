8dbefc7eee2a3d1998401b50c5555455b70dc615
noriyuki.shimizu0612@gmail.com <shiminori0612>
2024-03-17 00:57:06 +0900

    storybook の型を拡張

--- src/types/lib/index.d.ts
+++ src/types/lib/index.d.ts
@@ -1,5 +1,6 @@
 import './shims-vue'
 import './env'
+import './shims-@storybook-vue3'
 import 'vite-svg-loader/index.d'
 
 export {}
---
+++ src/types/lib/shims-@storybook-vue3.d.ts
@@ -0,0 +1,59 @@
+declare module '@storybook/vue3' {
+  import type { Conditional, SBScalarType, SBType } from '@storybook/vue3'
+
+  /** @see {@link https://storybook.js.org/docs/api/arg-types#controltype} */
+  type ControlType =
+    /* array or object */
+    | 'object'
+    /* boolean */
+    | 'boolean'
+    /* enum */
+    | 'check'
+    | 'inline-check'
+    | 'radio'
+    | 'inline-radio'
+    | 'select'
+    | 'multi-select'
+    /* number */
+    | 'number'
+    | 'range'
+    /* object */
+    | 'file'
+    /* string */
+    | 'color'
+    | 'date'
+    | 'text'
+    | null
+
+  /** @see {@link https://storybook.js.org/docs/api/arg-types#argtypes} */
+  interface ArgTypeObject {
+    argTypes: {
+      [key: string]: {
+        control?: ControlType | {
+          type: ControlType
+          accept?: string
+          labels?: { [option: string]: string }
+          max?: number
+          min?: number
+          presetColors?: string[]
+          step?: number
+        } | false
+        description?: string
+        if?: Conditional
+        mapping?: { [key: string]: { [option: string]: string } }
+        name?: string
+        options?: string[]
+        table?: {
+          category?: string
+          defaultValue?: { summary: string; detail?: string }
+          disable?: boolean
+          subcategory?: string
+          type?: { summary?: string; detail?: string }
+        }
+        type?: SBType | SBScalarType['name']
+      }
+    }
+  }
+}
+
+export {}
