2184adbd9d1414093aa7efbdc29a73a1dade7ec2
noriyuki.shimizu0612@gmail.com <shiminori0612>
2024-03-17 00:57:26 +0900

    Empty コンポーネントの story を追加

--- src/components/data-display/Empty/index.vue
+++ src/components/data-display/Empty/index.vue
@@ -1,12 +1,31 @@
+<script lang="ts">
+export default {
+  name: 'Empty'
+}
+</script>
+
 <script setup lang="ts">
 import EmptyIconSvg from '@/assets/svg/empty.svg?component'
+
+/** Props */
+const props = withDefaults(
+  defineProps<{
+    /** 空表示のイメージを表示するか */
+    noImage?: boolean
+  }>(),
+  {
+    noImage: false
+  }
+)
 </script>
 
 <template>
   <div :class="$style['empty']">
-    <slot name="image">
-      <EmptyIconSvg />
-    </slot>
+    <template v-if="!props.noImage">
+      <slot name="image">
+        <EmptyIconSvg />
+      </slot>
+    </template>
     <span :class="$style['empty__description']">
       <slot name="description">No data</slot>
     </span>
