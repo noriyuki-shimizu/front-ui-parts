e69eb7023ef1b4e285dedb24b8db18d113d24893
noriyuki.shimizu0612@gmail.com <shiminori0612>
2024-03-27 23:12:34 +0900

    eslint-plugin-vitest の導入

--- src/utils/core/lang.spec.ts
+++ src/utils/core/lang.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import {
   isArray,
   isBoolean,
@@ -14,77 +14,196 @@ import {
 } from './lang'
 
 describe('Lang utility functions', () => {
-  test('isNull', () => {
-    expect(isNull(null)).toBe(true)
-    expect(isNull(undefined)).toBe(false)
-    expect(isNull(0)).toBe(false)
-    expect(isNull('')).toBe(false)
-    expect(isNull([])).toBe(false)
-    expect(isNull({})).toBe(false)
-    expect(isNull(false)).toBe(false)
+  describe('isNull', () => {
+    it('value is null', () => {
+      expect(isNull(null)).toBe(true)
+    })
+
+    it('value is undefined', () => {
+      expect(isNull(undefined)).toBe(false)
+    })
+
+    it('value is 0', () => {
+      expect(isNull(0)).toBe(false)
+    })
+
+    it('value is ""', () => {
+      expect(isNull('')).toBe(false)
+    })
+
+    it('value is []', () => {
+      expect(isNull([])).toBe(false)
+    })
+
+    it('value is {}', () => {
+      expect(isNull({})).toBe(false)
+    })
+
+    it('value is false', () => {
+      expect(isNull(false)).toBe(false)
+    })
   })
 
-  test('isUndefined', () => {
-    expect(isUndefined(undefined)).toBe(true)
-    expect(isUndefined(null)).toBe(false)
-    expect(isUndefined(0)).toBe(false)
-    expect(isUndefined('')).toBe(false)
-    expect(isUndefined([])).toBe(false)
-    expect(isUndefined({})).toBe(false)
-    expect(isUndefined(false)).toBe(false)
+  describe('isUndefined', () => {
+    it('value is null', () => {
+      expect(isUndefined(null)).toBe(true)
+    })
+
+    it('value is undefined', () => {
+      expect(isUndefined(undefined)).toBe(false)
+    })
+
+    it('value is 0', () => {
+      expect(isUndefined(0)).toBe(false)
+    })
+
+    it('value is ""', () => {
+      expect(isUndefined('')).toBe(false)
+    })
+
+    it('value is []', () => {
+      expect(isUndefined([])).toBe(false)
+    })
+
+    it('value is {}', () => {
+      expect(isUndefined({})).toBe(false)
+    })
+
+    it('value is false', () => {
+      expect(isUndefined(false)).toBe(false)
+    })
   })
 
-  test('isNil', () => {
-    expect(isNil(null)).toBe(true)
-    expect(isNil(undefined)).toBe(true)
-    expect(isNil(0)).toBe(false)
-    expect(isNil('')).toBe(false)
-    expect(isNil([])).toBe(false)
-    expect(isNil({})).toBe(false)
-    expect(isNil(false)).toBe(false)
+  describe('isNil', () => {
+    it('value is null', () => {
+      expect(isNil(null)).toBe(true)
+    })
+
+    it('value is undefined', () => {
+      expect(isNil(undefined)).toBe(false)
+    })
+
+    it('value is 0', () => {
+      expect(isNil(0)).toBe(false)
+    })
+
+    it('value is ""', () => {
+      expect(isNil('')).toBe(false)
+    })
+
+    it('value is []', () => {
+      expect(isNil([])).toBe(false)
+    })
+
+    it('value is {}', () => {
+      expect(isNil({})).toBe(false)
+    })
+
+    it('value is false', () => {
+      expect(isNil(false)).toBe(false)
+    })
   })
 
-  test('isObject', () => {
-    expect(isObject({})).toBe(true)
-    expect(isObject(() => {})).toBe(true)
-    expect(isObject(null)).toBe(false)
-    expect(isObject(undefined)).toBe(false)
+  describe('isObject', () => {
+    it('value is {}', () => {
+      expect(isObject({})).toBe(true)
+    })
+
+    it('value is function', () => {
+      expect(isObject(() => {})).toBe(true)
+    })
+
+    it('value is null', () => {
+      expect(isObject(null)).toBe(false)
+    })
+
+    it('value is undefined', () => {
+      expect(isObject(undefined)).toBe(false)
+    })
   })
 
-  test('isNumber', () => {
-    expect(isNumber(123)).toBe(true)
-    expect(isNumber('123')).toBe(false)
-    expect(isNumber(null)).toBe(false)
+  describe('isNumber', () => {
+    it('value is string', () => {
+      expect(isNumber(123)).toBe(true)
+    })
+
+    it('value is number', () => {
+      expect(isNumber('123')).toBe(false)
+    })
+
+    it('value is null', () => {
+      expect(isNumber(null)).toBe(false)
+    })
   })
 
-  test('isString', () => {
-    expect(isString('123')).toBe(true)
-    expect(isString(123)).toBe(false)
-    expect(isString(null)).toBe(false)
+  describe('isString', () => {
+    it('value is string', () => {
+      expect(isString('123')).toBe(true)
+    })
+
+    it('value is number', () => {
+      expect(isString(123)).toBe(false)
+    })
+
+    it('value is null', () => {
+      expect(isNumber(null)).toBe(false)
+    })
   })
 
-  test('isBoolean', () => {
-    expect(isBoolean(true)).toBe(true)
-    expect(isBoolean(false)).toBe(true)
-    expect(isBoolean('true')).toBe(false)
+  describe('isBoolean', () => {
+    it('value is true', () => {
+      expect(isBoolean(true)).toBe(true)
+    })
+
+    it('value is false', () => {
+      expect(isBoolean(false)).toBe(true)
+    })
+
+    it('value is string', () => {
+      expect(isBoolean('true')).toBe(false)
+    })
   })
 
-  test('isArray', () => {
-    expect(isArray([1, 2, 3])).toBe(true)
-    expect(isArray('123')).toBe(false)
-    expect(isArray(null)).toBe(false)
+  describe('isArray', () => {
+    it('value is number[]', () => {
+      expect(isArray([1, 2, 3])).toBe(true)
+    })
+
+    it('value is string', () => {
+      expect(isArray('123')).toBe(false)
+    })
+
+    it('value is null', () => {
+      expect(isArray(null)).toBe(false)
+    })
   })
 
-  test('isMap', () => {
-    expect(isMap(new Map())).toBe(true)
-    expect(isMap({})).toBe(false)
-    expect(isMap(null)).toBe(false)
+  describe('isMap', () => {
+    it('value is Map', () => {
+      expect(isMap(new Map())).toBe(true)
+    })
+
+    it('value is {}', () => {
+      expect(isMap({})).toBe(false)
+    })
+
+    it('value is null', () => {
+      expect(isMap(null)).toBe(false)
+    })
   })
 
-  test('isSet', () => {
-    expect(isSet(new Set())).toBe(true)
-    expect(isSet({})).toBe(false)
-    expect(isSet(null)).toBe(false)
+  describe('isSet', () => {
+    it('value is Set', () => {
+      expect(isSet(new Set())).toBe(true)
+    })
+
+    it('value is {}', () => {
+      expect(isSet({})).toBe(false)
+    })
+
+    it('value is null', () => {
+      expect(isSet(null)).toBe(false)
+    })
   })
 
   describe('isEmpty', () => {
